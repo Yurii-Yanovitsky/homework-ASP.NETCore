1df6b12e09a38d88749bb1dcd4fd6df352e97591
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-02-19 16:39:16 +0200

    lesson-1: added additional task

---
+++ Lesson1/AdditionalTask/Controllers/ListController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdditionalTask.Controllers
+{
+    public class ListController : Controller
+    {
+        public IActionResult Info()
+        {
+            return View();
+        }
+    }
+}
