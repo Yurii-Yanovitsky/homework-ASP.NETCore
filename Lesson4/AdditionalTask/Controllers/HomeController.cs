f653ea83651aaf6764935ae00389ce7097f343db
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-02-25 01:32:19 +0200

    lesson-4: added additional task

---
+++ Lesson4/AdditionalTask/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdditionalTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult Page1()
+        {
+            return View();
+        }
+
+        public IActionResult Page2()
+        {
+            return View();
+        }
+    }
+}
