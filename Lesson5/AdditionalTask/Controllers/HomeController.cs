3a1f77b54063cc66a8c553c2dc7d081eb8112df1
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-03-06 23:25:51 +0200

    lesson-5: added additional task

---
+++ Lesson5/AdditionalTask/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdditionalTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string key = "myKey";
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(string value, DateTime dateTime)
+        {
+            CookieOptions options = new CookieOptions();
+            options.Expires = dateTime;
+
+            Response.Cookies.Append(key, value, options);
+
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            string value = Request.Cookies[key];
+
+            return View(value as object);
+        }
+
+    }
+}
