9abb5d721982d5a0d9f28a83f26f3329cd6cafd7
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-03-07 15:15:43 +0200

    lesson-6: added additional task

---
+++ Lesson6/AdditionalTask/Controllers/CalcController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdditionalTask.Controllers
+{
+    public class CalcController : Controller
+    {
+        private readonly CalculatorService _calculator;
+
+        public CalcController(CalculatorService calculator)
+        {
+            _calculator = calculator;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(double a, double b)
+        {
+
+            var result = _calculator.Add(a, b);
+
+            return Content(result.ToString());
+        }
+
+        [HttpPost]
+        public IActionResult Sub(double a, double b)
+        {
+            var result = _calculator.Sub(a, b);
+
+            return Content(result.ToString());
+        }
+
+        [HttpPost]
+        public IActionResult Mul(double a, double b)
+        {
+            var result = _calculator.Mul(a, b);
+
+            return Content(result.ToString());
+        }
+
+        [HttpPost]
+        public IActionResult Div(double a, double b)
+        {
+            var result = _calculator.Div(a, b);
+
+            if (result.IsValid)
+            {
+                return Content(result.Result.ToString());
+            }
+            else
+            {
+                return Content(result.Error);
+            }
+        }
+    }
+}
