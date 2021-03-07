cf86a069dfd3a6657be1781b06501f3203ff536e
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-03-07 15:16:15 +0200

    lesson-6: added task1

---
+++ Lesson6/Task1/Services/MonthService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Task1.Services
+{
+    public class MonthService : IReadOnlyCollection<string>
+    {
+        private string[] _months;
+
+        public MonthService()
+        {
+            _months = DateTimeFormatInfo.CurrentInfo.MonthNames;
+        }
+
+        public int Count => _months.Count();
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            foreach (var m in _months)
+            {
+                yield return m;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _months.GetEnumerator();
+        }
+    }
+}
---
+++ Lesson6/Task1/Services/WeekService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Task1.Services
+{
+    public class WeekService : IReadOnlyCollection<string>
+    {
+        private string[] _weeks;
+
+        public WeekService()
+        {
+            _weeks = DateTimeFormatInfo.CurrentInfo.DayNames;
+        }
+
+        public int Count => _weeks.Count();
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            foreach (var w in _weeks)
+            {
+                yield return w;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _weeks.GetEnumerator();
+        }
+    }
+}
